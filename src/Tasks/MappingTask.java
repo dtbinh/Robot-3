e78eff328627c67918875480f5d2f7191f730e41
Mert Tiftikci <merttiftikci@gmail.com>
2016-01-11 22:52:28 +0200

    Mapping starters with blue tooth connection...

---
+++ src/Tasks/MappingTask.java
@@ -0,0 +1,48 @@
+package Tasks;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import Modules.GyroSensor;
+import Modules.Pilot;
+import Modules.Radar;
+
+public class MappingTask implements Task {
+	
+	Radar radar;
+	Pilot pilot;
+	GyroSensor gyroSensor;
+
+	public MappingTask(Radar radar, Pilot pilot, GyroSensor gyroSensor) {
+		this.radar = radar;
+		this.pilot = pilot;
+		this.gyroSensor = gyroSensor;
+	}
+
+	@Override
+	public void startTask() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(1234);
+			Socket client = serverSocket.accept();
+			
+			OutputStream outputStream = client.getOutputStream();
+			DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+			
+			dataOutputStream.writeInt(0);
+			dataOutputStream.flush();
+
+			dataOutputStream.close();
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void resetTask() {
+		
+	}
+}
